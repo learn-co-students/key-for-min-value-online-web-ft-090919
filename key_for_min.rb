a682b73888b11fb75e6d3a4ca9771ea05d5974d9
= <=>
2019-09-11 12:43:48 -0400

    Done.

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  name_hash.reduce(nil) { |memo, (key, val)| memo == nil || val < name_hash[memo] ? key : memo }
 end
